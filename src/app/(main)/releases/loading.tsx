11223df455400b03f3c0e4b5e7217b02f9038802
iboughtbed <testpythontosanzhar@gmail.com>
2024-03-04 20:47:08 +0500

    update releases loading

--- src/app/(main)/releases/loading.tsx
+++ src/app/(main)/releases/loading.tsx
@@ -1,32 +1,3 @@
 export default function ReleasesLoading() {
-  return <></>;
-
-  // return (
-  //   <Shell>
-  //     <section className="flex min-h-screen flex-col flex-nowrap gap-8 md:flex-row">
-  //       <div className="relative md:w-1/2 md:px-8 xl:w-1/3">
-  //         <Card>
-  //           <div className="p-4">
-  //             <AspectRatio ratio={1 / 1.41}>
-  //               <PlaceholderImage isSkeleton asChild />
-  //             </AspectRatio>
-  //           </div>
-  //         </Card>
-  //       </div>
-  //       <div className="shrink-0 text-center md:w-1/2 md:text-left">
-  //         <Skeleton className="h-36 w-full" />
-  //         <Skeleton className="mt-6 h-12 w-full" />
-  //         <Skeleton className="mt-6 h-10 w-full" />
-  //       </div>
-  //     </section>
-
-  //     <section className="relative min-h-screen">
-  //       <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
-  //         {Array.from({ length: 8 }).map((_, i) => (
-  //           <ReleaseCardSkeleton key={i} />
-  //         ))}
-  //       </div>
-  //     </section>
-  //   </Shell>
-  // );
+  return <div className="relative min-h-screen w-full bg-background"></div>;
 }
