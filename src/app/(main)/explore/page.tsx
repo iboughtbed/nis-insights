0058653b2f2fc7d9a00d9c319eaf9b741360022f
iboughtbed <testpythontosanzhar@gmail.com>
2024-04-24 02:44:36 +0500

    fix: remove explore page header for mobile

--- src/app/(main)/explore/page.tsx
+++ src/app/(main)/explore/page.tsx
@@ -6,7 +6,7 @@ export default function ExplorePage() {
   return (
     <div className="container">
       <div className="flex flex-col">
-        <div className="relative overflow-hidden pb-10 pt-8">
+        <div className="relative overflow-hidden pb-10 pt-8 max-md:hidden">
           <div className="flex flex-col items-center">
             <h1 className="relative mt-6 text-center text-4xl font-bold leading-[3rem] tracking-tight max-md:text-balance sm:max-w-4xl sm:text-5xl sm:leading-[4rem] md:text-6xl md:leading-[4.5rem] lg:text-8xl">
               A new, easy <br />
