7142e03bd8f461aa926626ebbd2d548aa9c1bdf6
iboughtbed <testpythontosanzhar@gmail.com>
2024-09-14 20:21:10 +0500

    fix(next-auth): add callback to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,48 +1,63 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
-export default withAuth(function middleware(req) {
-  const token = req.nextauth.token;
-  const pathname = req.nextUrl.pathname;
+export default withAuth(
+  async function middleware(req) {
+    const token = req.nextauth.token;
+    const pathname = req.nextUrl.pathname;
 
-  const url = new URL(req.nextUrl.origin);
+    const url = new URL(req.nextUrl.origin);
 
-  if (pathname === "/") {
-    if (token?.sub) {
-      url.pathname = "/dashboard";
-      return NextResponse.redirect(url);
-    } else {
-      return NextResponse.next();
+    console.log({ pathname });
+
+    if (pathname === "/") {
+      console.log("main");
+
+      if (token?.sub) {
+        url.pathname = "/dashboard";
+        return NextResponse.redirect(url);
+      } else {
+        console.log("red");
+        return NextResponse.next();
+      }
     }
 
-
-  }
-
-  if (!token?.sub) {
-    url.pathname = "/";
-    return NextResponse.redirect(url);
-  }
-
-  if (
-    pathname.startsWith("/new/article") ||
-    pathname.startsWith("/edit/article")
-  ) {
-    if (token?.role !== "admin" && token.role !== "writer") {
+    if (!token?.sub) {
       url.pathname = "/";
       return NextResponse.redirect(url);
     }
-  }
 
-  if (
-    pathname.startsWith("/new/release") ||
-    pathname.startsWith("/edit/release")
-  ) {
-    if (token.role !== "admin") {
-      url.pathname = "/";
-      return NextResponse.redirect(url);
+    if (
+      pathname.startsWith("/new/article") ||
+      pathname.startsWith("/edit/article")
+    ) {
+      if (token?.role !== "admin" && token.role !== "writer") {
+        url.pathname = "/";
+        return NextResponse.redirect(url);
+      }
     }
-  }
-});
+
+    if (
+      pathname.startsWith("/new/release") ||
+      pathname.startsWith("/edit/release")
+    ) {
+      if (token.role !== "admin") {
+        url.pathname = "/";
+        return NextResponse.redirect(url);
+      }
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+        return true;
+      },
+    },
+  },
+);
 
 export const config = {
   matcher: ["/", "/new/:path*", "/edit/:path", "/dashboard/:path*"],
