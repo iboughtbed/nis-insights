c242502c2ff4663e9c9d58e6dfe632253f18d195
iboughtbed <testpythontosanzhar@gmail.com>
2025-01-25 22:59:13 +0500

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "~/styles/globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata, Viewport } from "next";
 
@@ -86,6 +87,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <TailwindIndicator />
           <Sonner richColors />
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
